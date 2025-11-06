a7d05eb7fab54954c4b6f64b9c83f333689d084c
masonralls <masonralls@mail.weber.edu>
2025-11-06 12:59:50 -0700

    Installed kernel and loaded the data into both notebooks

--- Notebooks/random_forest_model.py
+++ Notebooks/random_forest_model.py
@@ -1,6 +1,15 @@
+import os
 import kagglehub
+import numpy as np
+import pandas as pd
 
 # Download latest version
 path = kagglehub.dataset_download("ayeshaimran123/social-media-and-mental-health-balance")
 
 print("Path to dataset files:", path)
+
+# Load dataset
+file_path = os.path.join(path, 'Mental_Health_and_Social_Media_Balance_Dataset.csv')
+df = pd.read_csv(file_path)
+
+df.head()
